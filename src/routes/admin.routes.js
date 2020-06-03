b958578c568118475d779d59fa6b22b5424d6067
Johan Perez <davidjohan2@hotmail.com>
2020-06-03 15:09:21 -0500

    stiky citas-routes admin-navigation rol

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -25,13 +25,18 @@ const{
  //servicies------------------------------------------
 router.get('/admin/all_services', ListServices);
 router.get('/admin/new_service', createService);
+router.post('/admin/new_service', addService);
+//<------- cambio de estado---------->
+router.get('/admin/estatechange/:id', ChangeEstateService)
+
+//creacion de nuevos usuarios------------------------------
 router.get('/admin/new_veterinary',renderNVeterinary);
 router.get('/admin/new_admin',renderNAdmin);
-router.post('/admin/new_service', addService);
-router.get('/admin/estatechange/:id', ChangeEstateService)
+
+
 /////////------------------------------------------
 
-
+ //no existe
 //router.post('/animals/new_animal', createNewtype);
 //router.get('/admin/add_animal', rederNewanimal);
 
